080794428e2e40407e38338b40d916e70fb49a39
Andrew Sutherland <asutherland@asutherland.org>
2010-07-26 15:55:14 -0700

    early stages of at breaking; current tests pass but are fairly limited.

---
+++ tests/test-syntax.js
@@ -0,0 +1,87 @@
+var syn = require("narscribblus/scribble-syntax");
+
+var ALT_SYNTAX_MIRROR_EXPECTATIONS = [
+  ["|{", "}|"],
+  ["|<<<{", "}>>>|"],
+  ["|({", "})|"],
+  ["|[[{", "}]]|"],
+  ["|<>({", "})<>|"],
+];
+
+exports.altSyntaxMirror = function(test) {
+  for (var i = 0; i < ALT_SYNTAX_MIRROR_EXPECTATIONS.length; i++) {
+    var s = ALT_SYNTAX_MIRROR_EXPECTATIONS[i];
+    test.assertEqual(s[1], syn.altSyntaxMirror(s[0]), "mirroring " + s[0]);
+  }
+};
+
+var NESTED_COMMENT_EXPECTATIONS = [
+  ["@;{;}", 0, undefined],
+  ["@;{foo;}", 0, undefined],
+  ["  @;{foo;}  ", 2, -2],
+  ["x@;{@;{;};}x", 1, -1],
+  ["x@;{@;{@;{;}@;{;};};}x", 1, -1],
+];
+
+exports.nestedCommentWalker = function(test) {
+  for (var i = 0; i < NESTED_COMMENT_EXPECTATIONS.length; i++) {
+    var expected = NESTED_COMMENT_EXPECTATIONS[i];
+    var expectedEndex = (expected[2] === undefined) ? expected[0].length
+                          : expected[0].length + expected[2];
+    var actualEndex = syn.nestedCommentWalker(expected[0], expected[1]);
+    test.assertEqual(expectedEndex, actualEndex, "expected != actual");
+  }
+};
+
+var AT_BREAKER_EXPECTATIONS = [
+  // absolute basics
+  ["foo",
+   "foo"],
+  ["@foo",
+   [["foo", null, null]]],
+  ["@[foo]",
+   [[null, "foo", null]]],
+  ["@{foo}",
+   [[null, null, "foo"]]],
+  ["@foo[bar]",
+   [["foo", "bar", null]]],
+  ["@foo{baz}",
+   [["foo", null, "baz"]]],
+  ["@[bar]{baz}",
+   [[null, "bar", "baz"]]],
+  ["@foo[bar]{baz}",
+   [["foo", "bar", "baz"]]],
+  ["@foo[]{}",
+   [["foo", "", ""]]],
+
+  // basic sequence support
+  ["foo @bar baz",
+   ["foo ", ["bar", null, null], " baz"]],
+  ["@foo bar @baz",
+   [["foo", null, null], " bar ", ["baz", null, null]]],
+
+  // -- body
+  // whitespace is significant in bodies too
+  ["@foo{ bar }",
+   [["foo", null, " bar "]]],
+  // balanced braces are cool
+  ["@foo{{}}",
+   [["foo", null, "{}"]]],
+  ["@foo{{{}{}}{}}",
+   [["foo", null, "{{}{}}{}"]]],
+
+
+  // -- comments
+
+];
+
+exports.atBreaker = function(test) {
+  for (var i = 0; i < AT_BREAKER_EXPECTATIONS.length; i++) {
+    var expectation = AT_BREAKER_EXPECTATIONS[i];
+    var testString = expectation[0];
+    var oExpect = {val: expectation[1]};
+    var oResult = {val: syn.textStreamAtBreaker(testString)};
+    test.assertEqual(JSON.stringify(oExpect), JSON.stringify(oResult),
+                     "Test string: '" + testString + "' failure.");
+  }
+};
